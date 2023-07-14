d49fa074f04483ca35973d034eab0a2002e66ad4
Nelsonsalgado123 <93412229+Nelsonsalgado123@users.noreply.github.com>
2023-07-14 07:06:35 -0500

    Update areas.rb

--- Rubyscrip/areas.rb
+++ Rubyscrip/areas.rb
@@ -1,4 +1,27 @@
 
+=begin
+What is the difference between chomp and chop?
+En Perl, "chomp" y "chop" son dos funciones diferentes que se utilizan para manipular cadenas de texto. La función "chomp" se usa para eliminar el último carácter de una cadena si este es un carácter de nueva línea "\n". Por lo general, se utiliza después de leer una entrada de usuario o un archivo de texto para eliminar la nueva línea al final de la cadena.
+Por otro lado, la función "chop" se utiliza para eliminar el último carácter de una cadena, sin importar qué carácter sea. Por lo tanto, si la última letra es una letra o un espacio en blanco, también será eliminada. Se utiliza comúnmente para eliminar caracteres no deseados del final de una cadena.
+En resumen, "chomp" y "chop" son dos funciones diferentes en Perl que se utilizan para manipular cadenas de texto. La diferencia principal es que "chomp" elimina solo el último carácter de nueva línea de una cadena, mientras que "chop" elimina el último carácter de cualquier tipo de una cadena.
+
+Which are the different variables’ scopes and what are them used for?
+
+En la mayoría de los lenguajes de programación, incluyendo Java, Python y C++, existen diferentes tipos de ámbitos de variables que determinan dónde se puede acceder y utilizar una variable. Los ámbitos de variables se dividen en tres tipos principales: ámbito global, ámbito local y ámbito de bloque.
+Ámbito Global: Las variables con ámbito global son accesibles desde cualquier parte del programa, ya sea dentro o fuera de las funciones. Se definen fuera de cualquier función o bloque de código y su alcance se extiende a todo el programa. Se utilizan para definir valores constantes o datos que necesitan ser compartidos por diferentes partes del programa.
+Ámbito Local: Las variables con ámbito local solo son accesibles dentro de la función o método en el que se definen. No se pueden utilizar fuera de ese bloque de código. Las variables locales se utilizan comúnmente para almacenar información temporal o intermedia en una función o método.
+Ámbito de Bloque: En algunos lenguajes de programación, como C y C++, se puede definir un ámbito de bloque dentro de una función, donde las variables solo son accesibles dentro del bloque de código. Estas variables se conocen como variables de ámbito de bloque y solo pueden ser utilizadas dentro del bloque en el que se definen. Se utilizan para almacenar datos temporales o intermedios dentro de un bloque de código, como un bucle o una estructura condicional.
+
+
+A string is an object from the String class. What are the methods reverse, capitalize, and length for?
+
+En muchos lenguajes de programación, incluyendo Java y Python, una cadena de texto es un objeto de la clase String. La clase String proporciona varios métodos para manipular cadenas de texto, como reverse, capitalize y length.
+reverse(): Este método se utiliza para invertir el orden de los caracteres de una cadena. Es decir, el primer carácter se convierte en el último, el segundo en el penúltimo y así sucesivamente. Por ejemplo, si la cadena original es "hello", el resultado de aplicar reverse() sería "olleh".
+capitalize(): Este método se utiliza para convertir el primer carácter de una cadena en mayúscula y el resto en minúsculas. Por ejemplo, si la cadena original es "hello world", el resultado de aplicar capitalize() sería "Hello world".
+length(): Este método se utiliza para obtener la longitud de una cadena, es decir, el número de caracteres que contiene. Por ejemplo, si la cadena original es "hello", el resultado de aplicar length() sería 5.
+
+
+=end
 #Programa para calcular el área de un círculo:
 # Solicita al usuario que ingrese el radio del círculo
 puts "Ingrese el radio del círculo:"
